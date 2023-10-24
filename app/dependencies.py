35a111e2283b5a6c17ae41c107cbd56fb6a8d7f6
Gerbrand Bosch <gerbrandb@gmail.com>
2023-10-24 16:21:23 +0200

    Removed unused config var

--- app/dependencies.py
+++ app/dependencies.py
@@ -11,8 +11,6 @@ config.read("app.conf")
 register_base_url = config.get("app", "register_base_url")
 
 jwt_priv_key = load_jwk(config.get("app", "jwt_priv_key_path"))
-# TODO GB: Moet eruit
-jwt_pub_key = load_jwk(config.get("app", "jwt_pub_key_path"))
 
 jwt_crt_content = file_content_raise_if_none(config.get("app", "jwt_crt_path"))
 

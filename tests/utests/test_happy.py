0b64b9e5b383d895093cbf79bab363ab1d1588e8
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2024-01-30 16:32:38 +0100

    docs: added license to the repository

--- tests/utests/test_happy.py
+++ tests/utests/test_happy.py
@@ -2,9 +2,11 @@ def test_happy():
     happy = True
     assert happy
 
+
 def test_happy_happy():
     happy_happy = True
     assert happy_happy
+
 
 def test_happy_happy_happy():
     happy_happy_happy = True

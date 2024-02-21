506ab136ee343346a3fa269739e97569bdea502d
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2024-02-21 12:17:09 +0100

    refactor: refactored the login js file in preparation to split up the login steps

--- static/login.js
+++ static/login.js
@@ -1,29 +1,30 @@
-(function(){
-    addEventListener("submit", (event) => {
-        try{
-            let body = {}
-            for (let i = 0; i < event.target.elements.length; i++) {
-                if (event.target.elements[i].name !== "submit"){
-                    body[event.target.elements[i].name] = event.target.elements[i].value
-                }
-            }
-            let response = fetch("/submit", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body)
-            })
-            response.then(async (response) => {
-                if (response.status === 200) {
-                    let jsonResponse = await response.json()
-                    window.location.href = jsonResponse["redirect_url"]
-                } else {
-                    console.log("Error: " + response.statusText);
-                }
-            });
-        } finally {
-            event.preventDefault();
-        }
+async function handleOnSubmit(event) {
+    event.preventDefault();
+
+    const bsn = document.getElementById("uzi_id").value;
+    const state = document.getElementById("state").value;
+
+    const response = await fetch("/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "uzi_id": bsn,
+            "state": state,
+        }),
     });
-})();
+
+    if (response.status !== 200) {
+        console.log("Error: " + response.statusText);
+        return
+    }
+    const data = await response.json();
+
+    window.location.href = data["redirect_url"];
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form")
+    form.addEventListener("submit", handleOnSubmit)
+})
